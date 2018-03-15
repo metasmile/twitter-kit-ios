1108f72e26038acdcf514c177ed0e652a21c2d9c
marty-suzuki <s1180183@gmail.com>
2018-03-15 14:59:15 +0900

    update frameworks

--- carthage-build.sh
+++ carthage-build.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-if [ "${1}" = "" ]; then
+if [ "${1:?}" = "" ]; then
     echo "Must set module name"
     exit 1
 fi
 
-MODULE_NAME=${1}
+MODULE_NAME=${1:?}
 PROJ_DIR="$(cd $(dirname $0) && pwd)"
 WORKING_DIR="${PROJ_DIR}/${MODULE_NAME}"
-VERSION_NUM="$(cat ${PROJ_DIR}/versions | grep ${MODULE_NAME} | grep -oe '\d.\d.\d')"
+VERSIONS_PATH="${PROJ_DIR}/versions"
+
+if [ ! -f "${VERSIONS_PATH}" ]; then
+    echo "${VERSIONS_PATH} does not exist"
+    exit 2
+fi
+
+VERSION_NUM="$(cat ${VERSIONS_PATH} | grep ${MODULE_NAME} | grep -oe '\d.\d.\d')"
+
+if [ "${VERSION_NUM}" = "" ]; then
+    echo "${MODULE_NAME} version does not find in ${VERSIONS_PATH}"
+    exit 3
+fi
+
 PRODUCTS_DIR="${WORKING_DIR}/Products"
 CART_DIR="${WORKING_DIR}/Carthage"
 ZIP_DIR="${PRODUCTS_DIR}/${VERSION_NUM}"
@@ -37,17 +50,17 @@ rm -rf ${CART_DIR}
 # Generate JSON file
 JSON_PATH="${PROJ_DIR}/${MODULE_NAME}.json"
 URL="https://github.com/abema/twitter-kit-ios/raw/develop/${MODULE_NAME}/Products/${VERSION_NUM}/framework.zip"
-TMEP_PATH="${WORKING_DIR}/temp.json"
+TEMP_PATH="${WORKING_DIR}/temp.json"
 
 echo ""
 echo "*** Creating ${MODULE_NAME}.json ***"
 echo ""
 
-if [ -a "${TMEP_PATH}" ]; then
-    rm -f "${TMEP_PATH}"
+if [ -a "${TEMP_PATH}" ]; then
+    rm -f "${TEMP_PATH}"
 fi
 
-touch "${TMEP_PATH}"
+touch "${TEMP_PATH}"
 
 if [ -f "${JSON_PATH}" ]; then
     while read line
@@ -59,26 +72,26 @@ if [ -f "${JSON_PATH}" ]; then
             if [ "$(echo ${line} | grep ${VERSION_NUM})" = "" ]; then
 
                 if [ "${line}" = "{" ]; then
-                    echo "${line}" >> "${TMEP_PATH}"
+                    echo "${line}" >> "${TEMP_PATH}"
                 else
 
                     # If line does not have ',',  add to tail
                     if [ "$(echo ${line} | grep ,)" = "" ]; then
-                        echo -e "\t${line}," >> "${TMEP_PATH}"
+                        echo -e "\t${line}," >> "${TEMP_PATH}"
                     else
-                        echo -e "\t${line}" >> "${TMEP_PATH}"
+                        echo -e "\t${line}" >> "${TEMP_PATH}"
                     fi
                 fi
             fi
         fi
     done < "${JSON_PATH}"
 else
-    echo "{" >> "${TMEP_PATH}"
+    echo "{" >> "${TEMP_PATH}"
 fi
 
-echo -e "\t\"${VERSION_NUM}\": \"${URL}\"" >> "${TMEP_PATH}"
-echo "}" >> "${TMEP_PATH}"
+echo -e "\t\"${VERSION_NUM}\": \"${URL}\"" >> "${TEMP_PATH}"
+echo "}" >> "${TEMP_PATH}"
 
-mv "${TMEP_PATH}" "${JSON_PATH}"
+mv "${TEMP_PATH}" "${JSON_PATH}"
 
 echo "*** ${MODULE_NAME} build process finished ***"
